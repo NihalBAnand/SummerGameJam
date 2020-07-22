74abc38671507c26f8ef09cdc1fec34d243e6244
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2020-07-22 00:01:10 -0500

    Player Movement & Enemy Detection

--- SummerGameJam/Assets/Scripts/PlayerBattleController.cs
+++ SummerGameJam/Assets/Scripts/PlayerBattleController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -7,15 +8,26 @@ using UnityEngine.UIElements;
 
 public class PlayerBattleController : MonoBehaviour
 {
-    int speed = 6;
+    int speed = 600;
     int moved = 0;
 
+    public bool enemyAdj = false;
+
     public Text moveText;
+    public Text alert;
+
+    public List<string> enemyDirections = new List<string>();
+
+    public List<GameObject> enemies = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("enemy"))
+        {
+            enemies.Add(obj);
+        }
+        alert.enabled = false;
     }
 
     // Update is called once per frame
@@ -23,6 +35,7 @@ public class PlayerBattleController : MonoBehaviour
     {
         if (moved < speed)
             Movement();
+        DetectObjects();
         UpdateUI();
     }
 
@@ -30,28 +43,102 @@ public class PlayerBattleController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y);
-            moved++;
+            if (!enemyDirections.Contains("L"))
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y);
+                moved++;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y);
-            moved++;
+            if (!enemyDirections.Contains("R"))
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y);
+                moved++;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1);
-            moved++;
+            if (!enemyDirections.Contains("U"))
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1);
+                moved++;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1);
-            moved++;
+            if (!enemyDirections.Contains("D"))
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1);
+                moved++;
+            }
         }
     }
 
     void UpdateUI()
     {
         moveText.text = "Movement left: " + (speed - moved);
+        alert.enabled = enemyAdj;
+    }
+
+    void DetectObjects()
+    {
+        foreach (GameObject e in enemies)
+        {
+            if ((e.transform.position.x + 1 == gameObject.transform.position.x || e.transform.position.x - 1 == gameObject.transform.position.x) && (e.transform.position.y == gameObject.transform.position.y))
+            {
+                enemyAdj = true;
+            }
+            else if (e.transform.position.x == gameObject.transform.position.x && (e.transform.position.y + 1 == gameObject.transform.position.y || e.transform.position.y - 1 == gameObject.transform.position.y))
+            {
+                enemyAdj = true;
+            }
+            else
+            {
+                enemyAdj = false;
+            }
+            if (enemyAdj)
+            {
+                if (gameObject.transform.position.y > e.transform.position.y)
+                {
+                    enemyDirections.Add("D");
+                }
+                else if (enemyDirections.Contains("D"))
+                {
+                    enemyDirections.Remove("D");
+                }
+
+                if (gameObject.transform.position.y < e.transform.position.y)
+                {
+                    enemyDirections.Add("U");
+                }
+                else if (enemyDirections.Contains("U"))
+                {
+                    enemyDirections.Remove("U");
+                }
+
+                if (gameObject.transform.position.x > e.transform.position.x)
+                {
+                    enemyDirections.Add("L");
+                }
+                else if (enemyDirections.Contains("L"))
+                {
+                    enemyDirections.Remove("L");
+                }
+
+                if (gameObject.transform.position.x < e.transform.position.x)
+                {
+                    enemyDirections.Add("R");
+                }
+                else if (enemyDirections.Contains("R"))
+                {
+                    enemyDirections.Remove("R");
+                }
+            }
+            else
+            {
+                enemyDirections.Clear();
+            }
+        }
     }
 }
