ae15cbeae43b15ab1acd7ab08586242fb717684d
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2020-07-22 12:11:14 -0500

    tweaks

--- SummerGameJam/Assets/Scripts/EnemyBattleScript.cs
+++ SummerGameJam/Assets/Scripts/EnemyBattleScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class EnemyBattleScript : MonoBehaviour
 {
-    public int health = 100;
+    public int health = 50;
     public int dmg    = 10;
     public int speed  = 4;
     public int moved  = 0;
