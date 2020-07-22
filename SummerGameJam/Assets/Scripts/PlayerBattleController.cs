f4d2e173578e3c4562a8b733f13629bc0c533523
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2020-07-21 23:17:06 -0500

    Battle System ver 1.0

---
+++ SummerGameJam/Assets/Scripts/PlayerBattleController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class PlayerBattleController : MonoBehaviour
+{
+    int speed = 6;
+    int moved = 0;
+
+    public Text moveText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moved < speed)
+            Movement();
+        UpdateUI();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y);
+            moved++;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y);
+            moved++;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1);
+            moved++;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1);
+            moved++;
+        }
+    }
+
+    void UpdateUI()
+    {
+        moveText.text = "Movement left: " + (speed - moved);
+    }
+}
