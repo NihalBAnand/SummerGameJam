51225adb79509a56dd199486f0527cf8f5ef3bae
ansaws <junk.ansaws@gmail.com>
2020-07-23 16:32:48 -0500

    Update BattleStart.cs

--- SummerGameJam/Assets/Scripts/BattleStart.cs
+++ SummerGameJam/Assets/Scripts/BattleStart.cs
@@ -12,8 +12,6 @@ public class BattleStart : MonoBehaviour
 
 
     public GameObject enemy;
-    GameObject temp;
-    public GameObject enemie;
     public int enemies;
     public int ppl;
     int amount = 0;
@@ -47,9 +45,9 @@ public class BattleStart : MonoBehaviour
         amount = 0;
         while (amount < enemies)
         {
-            xpos = Random.Range(-5, 0)
+            xpos = Random.Range(-5, 0);
             ypos = Random.Range(-5, 0);
-            players.Add(Instantiate(enemie, new Vector3(xpos, ypos), Quaternion.identity));
+            players.Add(Instantiate(enemy, new Vector3(xpos, ypos), Quaternion.identity));
 
             amount++;
 
