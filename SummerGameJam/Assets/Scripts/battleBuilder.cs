eb297c392f51efa94ef7e154723b59da626dac49
ansaws <junk.ansaws@gmail.com>
2020-08-11 16:38:16 -0500

    created scene

---
+++ SummerGameJam/Assets/Scripts/battleBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class battleBuilder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string levelName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            parseToFile();
+        }
+    }
+
+    void parseToFile()
+    {
+
+    }
+}
