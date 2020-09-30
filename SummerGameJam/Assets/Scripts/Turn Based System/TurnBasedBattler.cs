11fe124de4943fa5fe20514b3fb42d352a191621
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2020-09-29 20:26:23 -0500

    Menu arrow works

--- SummerGameJam/Assets/Scripts/Turn Based System/TurnBasedBattler.cs
+++ SummerGameJam/Assets/Scripts/Turn Based System/TurnBasedBattler.cs
@@ -8,10 +8,13 @@ public class TurnBasedBattler : MonoBehaviour
     public int health;
     public bool doneLoading = false;
 
-    public string[] battleOptions = { "Attack", "Item", "Magic", "Run" };
     public string selected;
     public GameObject selectBox;
     public GameObject selectArrow;
+    public GameObject attackText;
+    public GameObject magicText;
+    public GameObject itemText;
+    public GameObject runText;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,10 +38,15 @@ public class TurnBasedBattler : MonoBehaviour
 
         selectBox = GameObject.FindGameObjectWithTag("selectbox");
         selectArrow = GameObject.Find("SelectArrow");
-        selectArrow.transform.position = new Vector3(-389.3f, 17.05f);
+        attackText = GameObject.Find("Attack");
+        magicText = GameObject.Find("Magic");
+        itemText = GameObject.Find("Item");
+        runText = GameObject.Find("Run");
+
+        selectArrow.transform.position = new Vector3(attackText.transform.position.x - 100f, attackText.transform.position.y,0);
         selectBox.SetActive(false);
         selectArrow.SetActive(false);
-        selected = battleOptions[0];
+        selected = "Attack";
 
     }
 
@@ -53,12 +61,12 @@ public class TurnBasedBattler : MonoBehaviour
                 if (selected == "Magic")
                 {
                     selected = "Attack";
-                    selectArrow.transform.position = new Vector3(-389.3f, 17.05f);
+                    selectArrow.transform.position = new Vector3(attackText.transform.position.x - 100f, attackText.transform.position.y, 0);
                 }
                 if (selected == "Run")
                 {
                     selected = "Item";
-                    selectArrow.transform.position = new Vector3(162f, 17.05f);
+                    selectArrow.transform.position = new Vector3(itemText.transform.position.x - 100f, itemText.transform.position.y, 0);
                 }
             }
             if (Input.GetKeyDown(KeyCode.DownArrow))
@@ -66,12 +74,38 @@ public class TurnBasedBattler : MonoBehaviour
                 if (selected == "Attack")
                 {
                     selected = "Magic";
-                    selectArrow.transform.position = new Vector3(-389.3f, -21.95f);
+                    selectArrow.transform.position = new Vector3(magicText.transform.position.x - 100f, magicText.transform.position.y, 0);
                 }
                 if (selected == "Item")
                 {
                     selected = "Run";
-                    selectArrow.transform.position = new Vector3(162f, -21.95f);
+                    selectArrow.transform.position = new Vector3(runText.transform.position.x - 100f, runText.transform.position.y, 0);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (selected == "Attack")
+                {
+                    selected = "Item";
+                    selectArrow.transform.position = new Vector3(itemText.transform.position.x - 100f, itemText.transform.position.y, 0);
+                }
+                if (selected == "Magic")
+                {
+                    selected = "Run";
+                    selectArrow.transform.position = new Vector3(runText.transform.position.x - 100f, runText.transform.position.y, 0);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (selected == "Item")
+                {
+                    selected = "Attack";
+                    selectArrow.transform.position = new Vector3(attackText.transform.position.x - 100f, attackText.transform.position.y, 0);
+                }
+                if (selected == "Run")
+                {
+                    selected = "Magic";
+                    selectArrow.transform.position = new Vector3(magicText.transform.position.x - 100f, magicText.transform.position.y, 0);
                 }
             }
             if (Input.GetKeyDown(KeyCode.Return))
